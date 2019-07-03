77cc0c99d8af31131eefdd9cb9c842889d43325e
maciejkorolik <maciej.korolik@gmail.com>
2019-07-03 15:37:25 +0200

    initial configuration and project structure

---
+++ crag-log-frontend/src/routes/index.js
@@ -0,0 +1,8 @@
+export const routes = {
+  home: '/',
+  login: '/login',
+  register: '/register',
+  climbs: '/climbs',
+  climb: '/climbs/details/:id',
+  stats: '/stats',
+};
---
+++ crag-log-frontend/src/views/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Root = () => (
+  <>
+    <div>hello world</div>
+    <div>hello world 2</div>
+  </>
+);
+
+export default Root;
