11efc1b413007364d0ba733bbc322ff86629e949
maciejkorolik <maciej.korolik@gmail.com>
2019-07-04 18:58:03 +0200

    NavButton done, storybook configured

---
+++ crag-log-frontend/src/components/atoms/NavButton/NavButton.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ active }) => (active ? '#C7D3AF' : 'transparent')};
+  text-decoration: none;
+  padding: 0;
+  width: 220px;
+  height: 70px;
+  border: none;
+  border-radius: 50px;
+  cursor: pointer;
+`;
+
+const StyledIcon = styled.div`
+  display: block;
+  width: 50px;
+  height: 40px;
+  margin-right: 15px;
+  background-image: url(${({ icon }) => icon});
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 100% 100%;
+  border: none;
+`;
+
+const StyledText = styled.div`
+  color: ${({ theme }) => theme.darkgrey};
+  font-family: 'Oswald', sans-serif;
+  line-height: 1;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-weight: ${({ theme }) => theme.light};
+`;
+
+const NavButton = ({ className, icon, children }) => (
+  <StyledWrapper active={className}>
+    <StyledIcon icon={icon} />
+    <StyledText>{children}</StyledText>
+  </StyledWrapper>
+);
+
+NavButton.propTypes = {
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  children: PropTypes.string,
+};
+
+NavButton.defaultProps = {
+  className: null,
+  icon: null,
+  children: null,
+};
+
+export default NavButton;
---
+++ crag-log-frontend/src/components/atoms/NavButton/NavButton.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+import routesIcon from 'assets/icons/routes.svg';
+import statsIcon from 'assets/icons/stats.svg';
+import NavButton from './NavButton';
+
+const GreenBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 300px;
+  background: #91a250;
+`;
+
+storiesOf('Atoms/NavButton', module)
+  .addDecorator(story => <GreenBackground>{story()}</GreenBackground>)
+  .add('Not active', () => <NavButton icon={routesIcon}>Routes</NavButton>)
+  .add('active', () => (
+    <NavButton className="active" icon={statsIcon}>
+      Stats
+    </NavButton>
+  ));
